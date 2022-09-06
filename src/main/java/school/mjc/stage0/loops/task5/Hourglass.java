9342117f9bac10392d7e09dd539a9702f727f2bc
nasty <anastaishastil2002@gmail.com>
2022-09-06 20:48:21 +0300

    Updated

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -13,7 +13,7 @@ public class Hourglass {
             System.out.println();
         }
 
-        for (int i = (int)Math.ceil(height/2.) + 1; i < height; i++) {
+        for (int i = (int)Math.ceil(height/2.) + 1; i <= height; i++) {
             for (int j = 0; j < height; j++) {
                 if (j < i && j > height - i - 1) {
                     System.out.print(8);
